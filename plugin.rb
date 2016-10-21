ae734a60ba31953c59e9c1dbda727c9ac55183f2
Vinoth Kannan <vinothkannan@vinkas.com>
2016-10-21 18:01:38 +0530

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-navigation
+# about: Custom navigation menu plugin for Discourse
+# version: 0.1
+# authors: Vinoth Kannan (vinothkannans)
+# url: https://github.com/codiss/discourse-navigation
